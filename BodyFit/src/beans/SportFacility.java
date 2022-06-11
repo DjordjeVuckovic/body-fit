a4eeed7d4717f1f3fcdb4256526e6c314b0fad3a
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-06-11 06:02:35 +0200

    model added , generic and idao implemeted

---
+++ BodyFit/src/beans/SportFacility.java
@@ -0,0 +1,18 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SportFacility {
+	private String sportFacilityId;
+	private String name;
+	private String type;
+	private ArrayList<String> facilityContent;
+	private boolean status;
+	private Location location;
+	private double averageGrade;
+	private Date openTime;
+	private Date closeTime;
+
+
+}
