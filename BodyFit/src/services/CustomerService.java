d5c63221218240262b0ba2178df0c9ef5a965e6b
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2022-06-19 13:05:36 +0200

    filterMe

--- BodyFit/src/services/CustomerService.java
+++ BodyFit/src/services/CustomerService.java
@@ -24,9 +24,7 @@ import beans.Customer;
 import dao.CustomerDao;
 import dto.UserDto;
 @Path("customers")
-@Provider
-@PreMatching
-public class CustomerService implements ContainerResponseFilter,ContainerRequestFilter{
+public class CustomerService {
 	CustomerDao customerDao = new CustomerDao();
 	
 	@Context
@@ -59,45 +57,7 @@ public class CustomerService implements ContainerResponseFilter,ContainerRequest
 		customerDao.create(customerNew);
 		return customerNew;
 	}
-	@Override
-	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-		if (request.getHeaderString("Origin") == null) {
-            return;
-        }
-
-        // If it is a preflight request, then we add all
-        // the CORS headers here.
-        if (isPreflightRequest(request)) {
-            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-            response.getHeaders().add("Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-            response.getHeaders().add("Access-Control-Allow-Headers",
-                // Whatever other non-standard/safe headers (see list above) 
-                // you want the client to be able to send to the server,
-                // put it in this list. And remove the ones you don't want.
-                "X-Requested-With, Authorization, " +
-                "Accept-Version, Content-MD5, CSRF-Token, Content-Type");
-        }
-
-        // Cross origin requests can be either simple requests
-        // or preflight request. We need to add this header
-        // to both type of requests. Only preflight requests
-        // need the previously added headers.
-        response.getHeaders().add("Access-Control-Allow-Origin", "*");
-    }
-		
-	@Override
-	public void filter(ContainerRequestContext request) throws IOException {
-		// TODO Auto-generated method stub
-		if (isPreflightRequest(request)) {
-            request.abortWith(Response.ok().build());
-            return;
-        }
-	}
-	private static boolean isPreflightRequest(ContainerRequestContext request) {
-        return request.getHeaderString("Origin") != null
-                && request.getMethod().equalsIgnoreCase("OPTIONS");
-    }
+	
 	
 
 }
