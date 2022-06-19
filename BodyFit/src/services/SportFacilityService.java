109a038f62bad0f1139984b792050b528e362cf8
Aleksa Zakic <zakic.ra79.2019@uns.ac.rs>
2022-06-19 06:24:37 +0200

    update

--- BodyFit/src/services/SportFacilityService.java
+++ BodyFit/src/services/SportFacilityService.java
@@ -1,6 +1,7 @@
 package services;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
@@ -11,12 +12,26 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
 
 import beans.SportFacility;
 import dao.SportFacilityDao;
 
 @Path("facilities")
-public class SportFacilityService {
+@Provider
+@PreMatching
+public class SportFacilityService implements ContainerResponseFilter,ContainerRequestFilter  {
 	
 	SportFacilityDao sportFacilityDao = new SportFacilityDao();
 	
@@ -48,5 +63,44 @@ public class SportFacilityService {
 		sportFacilityDao.setBasePath(getContext());
 		sportFacilityDao.create(SportFacility);
 	}
+	@Override
+	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
+		if (request.getHeaderString("Origin") == null) {
+            return;
+        }
+
+        // If it is a preflight request, then we add all
+        // the CORS headers here.
+        if (isPreflightRequest(request)) {
+            response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+            response.getHeaders().add("Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+            response.getHeaders().add("Access-Control-Allow-Headers",
+                // Whatever other non-standard/safe headers (see list above) 
+                // you want the client to be able to send to the server,
+                // put it in this list. And remove the ones you don't want.
+                "X-Requested-With, Authorization, " +
+                "Accept-Version, Content-MD5, CSRF-Token, Content-Type");
+        }
+
+        // Cross origin requests can be either simple requests
+        // or preflight request. We need to add this header
+        // to both type of requests. Only preflight requests
+        // need the previously added headers.
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+    }
+		
+	@Override
+	public void filter(ContainerRequestContext request) throws IOException {
+		// TODO Auto-generated method stub
+		if (isPreflightRequest(request)) {
+            request.abortWith(Response.ok().build());
+            return;
+        }
+	}
+	private static boolean isPreflightRequest(ContainerRequestContext request) {
+        return request.getHeaderString("Origin") != null
+                && request.getMethod().equalsIgnoreCase("OPTIONS");
+    }
 
 }
