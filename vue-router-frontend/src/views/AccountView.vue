2e72d8d0aed39cbc2b76f3d4593d16bf64883b0d
Aleksa Zakic <zakic.ra79.2019@uns.ac.rs>
2022-06-29 17:21:33 +0200

    account changing uptade

--- vue-router-frontend/src/views/AccountView.vue
+++ vue-router-frontend/src/views/AccountView.vue
@@ -4,20 +4,29 @@
         <h1>Account information</h1>
         <span>
             <label>Username:</label>
-            <p1 v-if="this.editBool">{{logedInUser.username}}
+            <div v-if="this.editBool" class="row">
+                <div  class="col-9">
+                    <p1 v-if="this.editBool">{{logedInUser.username}}</p1>
+                </div>
+                <div class="col-sm input">
+                    <svg  v-if="this.editBool" xmlns="http://www.w3.org/2000/svg" style="margin-left: 40%;" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16" @click="edit()">
+                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                    </svg>
+                </div>
+            </div >
             
-                <svg  v-if="this.editBool" xmlns="http://www.w3.org/2000/svg" style="margin-left: 85%;" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16" @click="edit()">
-                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-            </svg>
-            </p1>
+            
             <div v-if="!this.editBool" class="row">
                 
-                <div class="col-10">
+                <div class="col-9">
                     <input  v-model="logedInUser.username" type="text" required >
                 </div>
                 <div class="col-sm input">
-                    <svg  xmlns="http://www.w3.org/2000/svg"  width="20" height="20"  fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16" @click="edited()">
                         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin-left: 40%;" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16" @click="notEdited()">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                     </svg>
                 </div>
                 
@@ -26,19 +35,59 @@
         </span>
          <span>
             <label>Name:</label>
-            <p1>{{logedInUser.name}}
-            <svg xmlns="http://www.w3.org/2000/svg" style="margin-left: 85%;" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-            </svg>
-            </p1>
+            <div v-if="this.editNameBool" class="row">
+                <div  class="col-9">
+                    <p1 v-if="this.editNameBool">{{logedInUser.name}}</p1>
+                </div>
+                <div class="col-sm input">
+                    <svg  v-if="this.editNameBool" xmlns="http://www.w3.org/2000/svg" style="margin-left: 40%;" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16" @click="editName()">
+                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                    </svg>
+                </div>
+            </div >
+            <div v-if="!this.editNameBool" class="row">
+                
+                <div class="col-9">
+                    <input  v-model="logedInUser.name" type="text" required >
+                </div>
+                <div class="col-sm input">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16" @click="editedName()">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin-left: 40%;" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16" @click="notEditedName()">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                    </svg>
+                </div>
+                
+            </div>
         </span>
          <span>
             <label>Surname:</label>
-            <p1>{{logedInUser.surname}}
-            <svg xmlns="http://www.w3.org/2000/svg" style="margin-left: 85%;" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
-                <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-            </svg>
-            </p1>
+            <div v-if="this.editSurNameBool" class="row">
+                <div  class="col-9">
+                    <p1 v-if="this.editSurNameBool">{{logedInUser.surname}}</p1>
+                </div>
+                <div class="col-sm input">
+                    <svg  v-if="this.editSurNameBool" xmlns="http://www.w3.org/2000/svg" style="margin-left: 40%;" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16" @click="editSurName()">
+                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                    </svg>
+                </div>
+            </div >
+            <div v-if="!this.editSurNameBool" class="row">
+                
+                <div class="col-9">
+                    <input  v-model="logedInUser.surname" type="text" required >
+                </div>
+                <div class="col-sm input">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16" @click="editedSurName()">
+                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin-left: 40%;" width="20" height="20" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16" @click="notEditedSurName()">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                    </svg>
+                </div>
+                
+            </div>
         </span>
        
     
@@ -46,8 +95,11 @@
         <input  type="submit" class="submiter " value="Edit information"/>
         
     </form>
-</div>
     
+</div>
+    {{logedInUser.name}}
+    {{logedInUser.surname}}
+    {{logedInUser.username}}
 </template>
 
 <script>
@@ -56,12 +108,38 @@ export default {
     props:['logedInUser'],
     data(){
         return{
-            editBool: true
+            editBool: true,
+            editNameBool: true,
+            editSurNameBool: true
         }
     },
     methods:{
         edit(){
             this.editBool = false;
+        },
+        edited(){
+            this.editBool = true;
+        },
+        notEdited(){
+            this.editBool = true;
+        },
+        editName(){
+            this.editNameBool = false;
+        },
+        editedName(){
+            this.editNameBool = true;
+        },
+        notEditedName(){
+            this.editNameBool = true;
+        },
+        editSurName(){
+            this.editSurNameBool = false;
+        },
+        editedSurName(){
+            this.editSurNameBool = true;
+        },
+        notEditedSurName(){
+            this.editSurNameBool = true;
         }
     }
 }
