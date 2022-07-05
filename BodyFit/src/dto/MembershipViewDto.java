4c2bd35261c3e1f6fc4b6d024d80a0ea05e1e01e
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2022-07-05 11:41:37 +0200

    adding membership dto and service

---
+++ BodyFit/src/dto/MembershipViewDto.java
@@ -0,0 +1,30 @@
+package dto;
+
+import java.util.Date;
+
+import beans.Membership;
+import beans.MembershipType;
+
+public class MembershipViewDto {
+	public String type;
+	public Date paymentDate;
+	public Date dateAndTimeOfValidity;
+	public int price;
+	public String customerId;
+	public boolean isActive;
+	public int numberOfSession;
+	public MembershipViewDto(Membership membership) {
+		this.type = membership.getType().toString();
+		this.paymentDate = membership.getPaymentDate();
+		this.dateAndTimeOfValidity = membership.getDateAndTimeOfValidity();
+		this.price = membership.getPrice();
+		this.customerId = membership.getCustomerId();
+		this.isActive = membership.isActive();
+		this.numberOfSession = membership.getNumberOfSession();
+	}
+	public MembershipViewDto() {	
+	}
+
+	
+	
+}
