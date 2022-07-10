2b2111849f907016bde405c414e43c31d3bf22c5
ZakicAleksa <93937431+ZakicAleksa@users.noreply.github.com>
2022-07-10 03:02:22 +0200

    frontend syle update

--- vue-router-frontend/src/components/NavigationBar.vue
+++ vue-router-frontend/src/components/NavigationBar.vue
@@ -53,7 +53,13 @@
           </div>
           <div v-if="isManager">
             <li class="nav-item" id="mynav">
-              <router-link to="/addTrainingView">Add training</router-link>
+              <router-link to="/addTrainingView">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-calendar2-plus align-top mx-2" viewBox="0 0 16 16">
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+                  <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM8 8a.5.5 0 0 1 .5.5V10H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V11H6a.5.5 0 0 1 0-1h1.5V8.5A.5.5 0 0 1 8 8z"/>
+                </svg>
+                <span class="align-bottom">Add training</span>
+              </router-link>
             </li>
             <li class="nav-item dropdown  drop" >
               <a class="nav-link dropdown-toggle  drop nav-link active"  aria-current="page" style="::selection{background-color:#2691d9}" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
